9bb1a0565a8a6e5bae6cfec427b0bb69abed23a0
Jose Arrdondo <direccionjaa@gmail.com>
2022-12-04 14:35:56 +0100

    implement iterator_traits

--- iterator.hpp
+++ iterator.hpp
@@ -5,22 +5,55 @@
 
 namespace ft
 {
-  struct input_iterator_tag { };
-  struct output_iterator_tag { };
-  struct forward_iterator_tag : public input_iterator_tag { };
-  struct bidirectional_iterator_tag : public forward_iterator_tag { };
-  struct random_access_iterator_tag : public bidirectional_iterator_tag { };
+	// Tags of iterator type
+	struct input_iterator_tag { };
+	struct output_iterator_tag { };
+	struct forward_iterator_tag : public input_iterator_tag { };
+	struct bidirectional_iterator_tag : public forward_iterator_tag { };
+	struct random_access_iterator_tag : public bidirectional_iterator_tag { };
 
-	template <typename T>
+	// ITERATOR_TRAITS
+	template <class Iter>
+	struct iterator_traits
+	{
+		typedef typename Iter::difference_type		difference_type;
+		typedef typename Iter::value_type			value_type;
+		typedef typename Iter::pointer				pointer;
+		typedef typename Iter::reference			reference;
+		typedef typename Iter::iterator_category	iterator_category;
+	}
+
+	template <class T>
+	struct iterator_traits<T*>
+	{
+		typedef std::ptrdiff_t					difference_type;
+		typedef T								value_type;
+		typedef T*								pointer;
+		typedef T&								reference;
+		typedef ft::random_access_iterator_tag	iterator_category;
+	}
+
+	template <class T>
+	struct iterator_traits<const T*>
+	{
+		typedef std::ptrdiff_t					difference_type;
+		typedef T								value_type;
+		typedef const T*						pointer;
+		typedef const T&						reference;
+		typedef ft::random_access_iterator_tag	iterator_category;
+	}
+
+	// RANDOM ACCESS ITERATOR
+	template <typename It>
 	class RandomAccessIterator
 	{
 		// MEMBER TYPES
 	public:
-		typedef ft::random_access_iterator_tag	iterator_category;
-		typedef T								value_type;
-		typedef std::ptrdiff_t						difference_type;
-		typedef T*								pointer;
-		typedef T&								reference;
+		typedef typename ft::iterator_traits<It*>::value_type		value_type;
+		typedef typename ft::iterator_traits<It*>::difference_type	difference_type;
+		typedef typename ft::iterator_traits<It*>::pointer			pointer;
+		typedef typename ft::iterator_traits<It*>::reference		reference;
+		typedef ft::random_access_iterator_tag						iterator_category;
 
 		// DATA MEMBERS
 	private:
