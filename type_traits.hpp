a69ecb4e0a2c72ca7a0f70595175047d3ca8e2bc
Jose Arrdondo <direccionjaa@gmail.com>
2022-12-08 00:15:16 +0100

    implementing utilities
    enable_if, is_integral, equal, lexicographical_compare, pair,``
    make_pair

---
+++ type_traits.hpp
@@ -0,0 +1,61 @@
+#ifndef TYPE_TRAITS_HPP
+# define TYPE_TRAITS_HPP
+
+namespace ft
+{
+	// ENABLE_IF
+	template <bool B, class T = void>
+	struct enable_if {};
+
+	template <class T>
+	struct enable_if<true, T> { typedef T type; };
+
+	// IS_INTEGRAL
+	template <class T>
+	struct is_integral { static const bool value = false; };
+
+	template <>
+	struct is_integral<bool> { static const bool value = true; };
+
+	template <>
+	struct is_integral<char> { static const bool value = true; };
+
+	template <>
+	struct is_integral<char16_t> { static const bool value = true; };
+
+	template <>
+	struct is_integral<char32_t> { static const bool value = true; };
+
+	template <>
+	struct is_integral<wchar_t> { static const bool value = true; };
+
+	template <>
+	struct is_integral<short> { static const bool value = true; };
+
+	template <>
+	struct is_integral<int> { static const bool value = true; };
+
+	template <>
+	struct is_integral<long> { static const bool value = true; };
+
+	template <>
+	struct is_integral<long long> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned char> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned short> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned int> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned long> { static const bool value = true; };
+
+	template <>
+	struct is_integral<unsigned long long> { static const bool value = true; };
+
+}
+
+#endif
