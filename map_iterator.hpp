b98d222cd25e1fe618a92e660d27df8fda3dbbd2
Jose Arrdondo <direccionjaa@gmail.com>
2023-02-05 20:51:50 +0100

    map_iterator increment and decrement

--- map_iterator.hpp
+++ map_iterator.hpp
@@ -13,6 +13,57 @@ namespace ft
 		node	*parent;
 		node	*left;
 		node	*right;
+
+		node(T val = T()) : value(val), parent(NULL), left(NULL), right(NULL) {}
+		node(const node& other) { *this = other; }
+		~node() {}
+
+		node& operator=(const node& other) {
+			value = other.value;
+			parent = other.parent;
+			left = other.left;
+			right = other.right;
+		}
+
+		node *minimum(node *n) {
+			if (!n)
+				return NULL;
+			while (n->left)
+				n = n->left;
+			return n;
+		}
+		node *maximum(node *n) {
+			if (!n)
+				return NULL;
+			while (n->right)
+				n = n->right;
+			return n;
+		}
+
+		node *next() {
+			node *tmp = this;
+			if (tmp->right)
+				return minimun(tmp->right);
+
+			node *tmp_parent = tmp->parent;
+			while (tmp_parent && tmp == parent.right) {
+				tmp = tmp_parent;
+				tmp_parent = tmp_parent->parent;
+			}
+			return tmp_parent;
+		}
+		node *prev() {
+			node *tmp = this;
+			if (tmp->left)
+				return maximum(tmp->left);
+
+			node *tmp_parent = tmp->parent;
+			while (tmp_parent && tmp == parent.left) {
+				tmp = tmp_parent;
+				tmp_parent = tmp_parent->parent;
+			}
+			return tmp_parent;
+		}
 	};
 
 	template <typename T>
@@ -51,18 +102,17 @@ namespace ft
 		reference operator*() const { return _ptr->value; }
 		pointer operator->() const { return &(_ptr->value); }
 
-		// TODO: increment and decrement operator (_ptr->next() and _ptr->prev())
-		mapIterator& operator++() {this->_ptr++; return *this; }
+		mapIterator& operator++() {_ptr = _ptr->next(); return *this; }
 		mapIterator operator++(int) {
 			mapIterator tmp(*this);
-			this->_ptr++;
+			_ptr = _ptr->next();
 			return tmp;
 		}
 
-		mapIterator& operator--() {this->_ptr--; return *this; }
+		mapIterator& operator--() {_ptr = _ptr->prev(); return *this; }
 		mapIterator operator--(int) {
 			mapIterator tmp(*this);
-			this->_ptr--;
+			_ptr = _ptr->prev();
 			return tmp;
 		}
 
